e997b3daf04eb711027393dd34858835ee368c21
Thalesousa <thalestjsb@gmail.com>
2020-11-15 12:04:26 -0300

    Exibindo erros no Input

--- Nivel-3/gobarber-web/src/components/Input/index.tsx
+++ Nivel-3/gobarber-web/src/components/Input/index.tsx
@@ -1,18 +1,18 @@
 import React, {
   InputHTMLAttributes,
-  useCallback,
   useEffect,
   useRef,
   useState,
+  useCallback,
 } from 'react';
+import { IconBaseProps } from 'react-icons';
 import { useField } from '@unform/core';
 
-import { IconBaseProps } from 'react-icons';
 import { Container } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
-  icon: React.ComponentType<IconBaseProps>;
+  icon?: React.ComponentType<IconBaseProps>;
 }
 
 const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
@@ -41,14 +41,17 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, [fieldName, registerField]);
 
   return (
-    <Container isFilled={isFilled} isFocused={isFocused}>
+    <Container isFocused={isFocused} isFilled={isFilled}>
       {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
         defaultValue={defaultValue}
+        ref={inputRef}
         {...rest}
+        type="text"
       />
+      {error}
     </Container>
   );
 };

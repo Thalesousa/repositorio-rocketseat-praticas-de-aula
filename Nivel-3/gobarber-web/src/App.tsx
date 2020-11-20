13ad7307f51d63fffeb71d1cf010ecee56b4a6e3
Thalesousa <thalestjsb@gmail.com>
2020-11-20 09:09:44 -0300

    Estrutura de Toasts

--- Nivel-3/gobarber-web/src/App.tsx
+++ Nivel-3/gobarber-web/src/App.tsx
@@ -5,12 +5,15 @@ import SignIn from './pages/Signin';
 import GlobalStyle from './styles/global';
 
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyle />
   </>

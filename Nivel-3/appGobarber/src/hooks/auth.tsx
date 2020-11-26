2356dfe6aaaea079b9fc3b3c157deae549e6ea7e
Thalesousa <thalestjsb@gmail.com>
2020-11-26 13:04:33 -0300

    Rotas privadas

--- Nivel-3/appGobarber/src/hooks/auth.tsx
+++ Nivel-3/appGobarber/src/hooks/auth.tsx
@@ -21,6 +21,7 @@ interface SignInCredentials {
 
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
@@ -29,6 +30,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStorageData(): Promise<void> {
@@ -40,9 +42,9 @@ const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
-
-      loadStorageData();
+      setLoading(false);
     }
+    loadStorageData();
   }, []);
 
   const signIn = useCallback(async ({ email, password }) => {
@@ -68,7 +70,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );

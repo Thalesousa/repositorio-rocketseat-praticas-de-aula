9437cfac862d3c62a669b02e2574e7d8ace18015
Thalesousa <thalestjsb@gmail.com>
2021-02-04 09:09:39 -0300

    Refatorando módulo de usuários

--- Nivel-2/primeiro-projeto-node/src/modules/users/infra/http/routes/users.routes.ts
+++ Nivel-2/primeiro-projeto-node/src/modules/users/infra/http/routes/users.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
 
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import CreateUserService from '@modules/users/services/CreateUserService';
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
@@ -12,8 +13,9 @@ const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   const { name, email, password } = request.body;
+  const usersRepository = new UsersRepository();
 
-  const createUser = new CreateUserService();
+  const createUser = new CreateUserService(usersRepository);
 
   const user = await createUser.execute({
     name,
@@ -38,7 +40,9 @@ usersRouter.patch(
   upload.single('avatar'),
   async (request, response) => {
     try {
-      const updateUserAvatar = new UpdateUserAvatarService();
+      const usersRepository = new UsersRepository();
+
+      const updateUserAvatar = new UpdateUserAvatarService(usersRepository);
       const user = await updateUserAvatar.execute({
         user_id: request.user.id,
         avatarFilename: request.file.filename,

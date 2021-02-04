fe2153cee39cc593b1a66d19379be0323b6211a6
Thalesousa <thalestjsb@gmail.com>
2021-02-04 16:03:27 -0300

    Criando o primeiro teste

--- Nivel-2/primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentServices.spec.ts
+++ Nivel-2/primeiro-projeto-node/src/modules/appointments/services/CreateAppointmentServices.spec.ts
@@ -1,8 +1,20 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentServices';
 
 describe('CreateAppointmentService', () => {
-  it('should be able to create a new appointment', () => {
-    expect(1 + 2).toBe(3);
+  it('should be able to create a new appointment', async() => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment= await createAppointment.execute({
+      date: new Date(),
+      provider_id: '1231213121231',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('1231213121231');
   });
 
   // it('should not be able to create two appointment on the same time', () => {
